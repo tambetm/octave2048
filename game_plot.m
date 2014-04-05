2de097d4a72b6e0f12679c71f1f9cb33b19a9eca
Tambet Matiisen <tambet.matiisen@gmail.com>
2014-04-05 08:35:50 +0300

    Code to run and plot the results of 4 sample algorithms

--- game_plot.m
+++ game_plot.m
@@ -1,13 +1,30 @@
+random_results = game_random(100);
+corner_results = game_corner(100);
+random3_results = game_random3(100);
+greedy_results = game_greedy(100);
+
 clf;
 
-subplot(2,1,1);
+subplot(2,2,1);
 hist(random_results);
 title('Random algorithm');
 #xlabel('Score');
 ylabel('Nr of trials');
 
-subplot(2,1,2);
+subplot(2,2,2);
 hist(corner_results);
 title('Corner algorithm');
+#xlabel('Score');
+ylabel('Nr of trials');
+
+subplot(2,2,3);
+hist(random3_results);
+title('Random3 algorithm');
+xlabel('Score');
+ylabel('Nr of trials');
+
+subplot(2,2,4);
+hist(greedy_results);
+title('Greedy algorithm');
 xlabel('Score');
 ylabel('Nr of trials');
