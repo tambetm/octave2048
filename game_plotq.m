1d6a3986db0514fea0e606c272dbfed3e9c9a7aa
Tambet Matiisen <tambet.matiisen@gmail.com>
2014-04-13 15:15:02 +0300

    Apply some polishing to diagram

--- game_plotq.m
+++ game_plotq.m
@@ -2,4 +2,6 @@ results = game_qtable(10000, 1, 0.9, 0.2);
 resultsmean = accumarray((idivide((1:10000).-1, 1000, 'floor').+1)',results, [], @mean);
 plot(resultsmean);
 set(gca,'xtick',(1:10));
-set(gca,'xticklabel',(0:1000:10000));
+set(gca,'xticklabel',(1000:1000:10000));
+xlabel('Nr of trials')
+ylabel('Average score');
